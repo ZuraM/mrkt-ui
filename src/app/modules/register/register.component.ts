501767a4c1b584ea53acefc690e68bbcac8ad13a
Zura Maisuradze <zuramaisuradze77@gmail.com>
2019-06-14 18:58:17 +0400

    password validation

--- src/app/modules/register/register.component.ts
+++ src/app/modules/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from '../../shared/validators';
 
 @Component({
   selector: 'mrkt-register',
@@ -12,18 +13,23 @@ export class RegisterComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.registerForm = this.fb.group({
-      email: ['', Validators.compose([Validators.email, Validators.required])],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
-      fullName: ['', Validators.required],
-      companyName: ['', Validators.required],
-      position: ['', Validators.required],
-      city: ['', Validators.required],
-      bussinessPhone: ['', Validators.required],
-      bussinessMail: ['', Validators.required],
-      terms: [true, Validators.requiredTrue]
-    });
+    this.registerForm = this.fb.group(
+      {
+        email: ['', Validators.compose([Validators.email, Validators.required])],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+        fullName: ['', Validators.required],
+        companyName: ['', Validators.required],
+        position: ['', Validators.required],
+        city: ['', Validators.required],
+        bussinessPhone: ['', Validators.required],
+        bussinessMail: ['', Validators.compose([Validators.required, Validators.email])],
+        terms: [true, Validators.requiredTrue]
+      },
+      {
+        validator: MustMatch('password', 'confirmPassword')
+      }
+    );
   }
 
   onSubmitRegister() {
